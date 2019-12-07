01049059cb4d07729249e3ed0468af3e6bdcf1c1
rc1021 <rc.701021@gmail.com>
2019-12-07 17:26:02 +0800

    加入留言功能

--- dist/js/stock_calc/main.js
+++ dist/js/stock_calc/main.js
@@ -195,8 +195,6 @@ $('#share').on('click', function (event) {
     }
 });
 
-commentBox('5765278868701184-proj');
-
 $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('.scroll-top').fadeIn();
@@ -212,3 +210,17 @@ window.onpopstate = function(event){
         calc.call($('form').get(0));
     }
 };
+
+// make sure the id is yours
+window.gc_params = {
+    graphcomment_id: 'rc1021-github-io',
+    // if your website has a fixed header, indicate it's height in pixels
+    fixed_header_height: 0,
+};
+
+/* - - - DON'T EDIT BELOW THIS LINE - - - */
+(function() {
+    var gc = document.createElement('script'); gc.type = 'text/javascript'; gc.async = true;
+    gc.src = 'https://graphcomment.com/js/integration.js?' + Math.round(Math.random() * 1e8);
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(gc);
+})();
